dfa2ff92525a985ee1ab498c779d4a35444d83f4
Frank Mullenger <frankmullenger@gmail.com>
2011-11-08 17:48:11 +1300

    Documentation.

--- code/customer/CustomerDecorator.php
+++ code/customer/CustomerDecorator.php
@@ -1,7 +1,22 @@
 <?php
-
+/**
+ * Add some fields to customer to facilitate the Payment class - mostly address related fields.
+ * Payment class requires these fields in order to pass address information to the payment gateway.
+ * 
+ * @author Frank Mullenger <frankmullenger@gmail.com>
+ * @copyright Copyright (c) 2011, Frank Mullenger
+ * @package shop
+ * @subpackage customer
+ * @version 1.0
+ */
 class CustomerDecorator extends DataObjectDecorator {
 
+  /**
+   * Extra DB fields, mostly for address - requirements for the Payment class.
+   * Also ties Member class with Address and Order classes.
+   * 
+   * @see DataObjectDecorator::extraStatics()
+   */
 	function extraStatics() {
 		return array(
 			'db' => array(
@@ -21,12 +36,22 @@ class CustomerDecorator extends DataObjectDecorator {
 		);
 	}
 
+	/**
+	 * Add some fields for managing Members in the CMS.
+	 * 
+	 * @see DataObjectDecorator::updateCMSFields()
+	 */
 	function updateCMSFields($fields) {
 		$fields->removeByName('Country');
 		$fields->addFieldToTab('Root.Main', new DropdownField('Country', 'Country', Geoip::getCountryDropDown()));
 		$fields->removeByName('Notes');
 	}
 	
+	/**
+	 * Retrieve the last used billing address for this Member from their previous saved addresses.
+	 * 
+	 * @return Address The last billing address
+	 */
   function BillingAddress() {
 	  $address = null;
 
@@ -39,6 +64,11 @@ class CustomerDecorator extends DataObjectDecorator {
 	  return $address;
 	}
 	
+	/**
+	 * Retrieve the last used shipping address for this Member from their previous saved addresses.
+	 * 
+	 * @return Address The last shipping address
+	 */
   function ShippingAddress() {
 	  $address = null;
 
@@ -52,6 +82,8 @@ class CustomerDecorator extends DataObjectDecorator {
 	
 	/**
 	 * Overload getter to return only non-cart orders
+	 * 
+	 * @return DataObjectSet Set of previous orders for this member
 	 */
 	function Orders() {
 	  return DataObject::get('Order', "`MemberID` = " . $this->owner->ID . " AND `Order`.`Status` != 'Cart'", "`Created` DESC");
