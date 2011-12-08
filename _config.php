96039407325457b7d7ce44a0bdb899472011300c
Frank Mullenger <frankmullenger@gmail.com>
2011-12-09 11:28:50 +1300

    Removing dependency on DOM from shop config.

--- _config.php
+++ _config.php
@@ -8,14 +8,10 @@ Object::add_extension('SiteConfig', 'ShopSettings');
 //Extend page controller
 Object::add_extension('Page_Controller', 'CartControllerExtension');
 
-//Allow product images to be sorted
-SortableDataObject::add_sortable_classes(array('ProductImage'));
-
 //Redirect customers logging in to the account page
 Security::set_default_login_dest('account');
 
 //Rules for product links
-
 Director::addRules(50, array( 
   //'product//$ID' => 'Product_Controller',
   'product//$ID/$Action' => 'Product_Controller'
