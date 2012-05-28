9b0aecc82756cf9b07234a382f5e09017402a592
Frank Mullenger <frankmullenger@gmail.com>
2012-05-28 16:59:43 +1200

    MINOR: Attribute options.

--- code/product/Product.php
+++ code/product/Product.php
@@ -1075,13 +1075,14 @@ class Product_Controller extends Page_Controller {
     if ($variations && $variations->exists()) foreach ($variations as $variation) {
 
       $variationOptions = array();
-      //if ($attributeOptions && is_array($attributeOptions)) 
-      foreach ($attributeOptions as $attributeID => $optionID) {
-        
-        //Get option for attribute ID, if this variation has options for every attribute in the array then add it to filtered
-        $attributeOption = $variation->getOptionForAttribute($attributeID);
-        if ($attributeOption && $attributeOption->ID == $optionID) $variationOptions[$attributeID] = $optionID;
-      }
+      //if ($attributeOptions && is_array($attributeOptions)) {
+        foreach ($attributeOptions as $attributeID => $optionID) {
+          
+          //Get option for attribute ID, if this variation has options for every attribute in the array then add it to filtered
+          $attributeOption = $variation->getOptionForAttribute($attributeID);
+          if ($attributeOption && $attributeOption->ID == $optionID) $variationOptions[$attributeID] = $optionID;
+        }
+      //}
       
       if ($variationOptions == $attributeOptions && $variation->isEnabled()) {
         $filteredVariations->push($variation);
