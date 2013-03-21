999cfeee21d99eed617bb1ad8d43b8f532591e45
James Bolitho <jim2mullered@hotmail.com>
2013-03-21 09:59:52 +0000

    Update Order.php

--- code/order/Order.php
+++ code/order/Order.php
@@ -460,7 +460,7 @@ class Order extends DataObject implements PermissionProvider {
 	
 	/**
 	 * Calculate the total outstanding for this order that remains to be paid,
-	 * all payments except 'Failure' payments are considered
+	 * all payments except 'Failure'  && 'Pending' payments are considered
 	 * 
 	 * @return Money With value and currency of total outstanding
 	 */
