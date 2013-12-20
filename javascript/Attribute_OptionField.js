171066f99d9c225e799f3d7e4519ea9219cb53a2
Frank Mullenger <frank@silverstripe.com>
2013-12-20 15:07:55 +1300

    BUGFIX: Product options not exactly matching available variations, refs #23.

--- javascript/Attribute_OptionField.js
+++ javascript/Attribute_OptionField.js
@@ -1,14 +1,14 @@
 ;(function($) {
+
 	$.entwine('sws', function($){
 
 		$('.attribute_option select').entwine({
 
 			onmatch : function() {
-				var self = this;
+				var self = this,
+					prev = $('select[name="' + this.data('prev') + '"]');
 
 				//If prev, prev.on change update these options
-				var prev = $('select[name="' + this.data('prev') + '"]');
-
 				if (prev.length) {
 					prev.on('change', function(e) {
 						self._updateOptions(e);
@@ -22,16 +22,54 @@
 			},
 
 			_updateOptions: function(e) {
-				var self = this;
-				var options = this.data('map')[$(e.currentTarget).val()];
+				var self = this,
+					options = this.data('map')[$(e.currentTarget).val()],
+					prev = $('select[name="' + this.data('prev') + '"]'),
+					partial = [],
+					variations = $('#ProductForm_ProductForm').data('map');
 
 				$('option', this).remove();
-				if (options != null) {
+				if (options !== null) {
 					$.each(options, function(val, text) {
-						$("<option/>").attr("value", val).html(text).appendTo(self);
+
+						if (prev.length) {
+
+							// Partial variation options
+							partial = [];
+							prev._getOptions(partial);
+							partial.push(val);
+
+							// loop through the variations and find if match exists
+							for (var key in variations) {
+								if(variations.hasOwnProperty(key)){
+
+									var variationOptions = variations[key]['options'];
+									if (variationOptions.filter(function(elem) {
+										return partial.indexOf(elem) > -1;
+									}).length == partial.length) {
+										$("<option/>").attr("value", val).html(text).appendTo(self);
+									}
+								}
+							}
+						}
+						else {
+							$("<option/>").attr("value", val).html(text).appendTo(self);
+						}
 					});
 				}
 				this.change();
+			},
+
+			_getOptions: function(partial) {
+
+				var prev = $('select[name="' + this.data('prev') + '"]');
+				if (prev.length) {
+					prev._getOptions(partial);
+				}
+				var val = $(this).val();
+				if (val) {
+					partial.push(val);
+				}
 			}
 		});
 
