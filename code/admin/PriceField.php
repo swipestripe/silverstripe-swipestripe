2d00c6aa16de1bd1da0db077f450735d65f06b8b
elliot sawyer (remote) <elliot@silverstripe.com>
2014-06-15 22:41:47 +1200

    BUG Money precision needs to accept less than 2 decimal places

--- code/admin/PriceField.php
+++ code/admin/PriceField.php
@@ -34,7 +34,7 @@ class PriceField extends CurrencyField {
 		if(!$val) $val = 0.00;
 		$shopConfig = ShopConfig::current_shop_config();
 		$precision = 2;		//precision should always be two decimals, and only more if specified in ShopConfig
-		if($shopConfig && $shopConfig->BaseCurrencyPrecision > 2) {
+		if($shopConfig && (int) $shopConfig->BaseCurrencyPrecision) {
 			$precision = $shopConfig->BaseCurrencyPrecision;
 		}
 
